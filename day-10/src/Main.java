24c465d7cae896dec3b72cf4ca42de172f666c1e
Utkarsh Sharma <utkarshsharma366@gmail.com>
2024-02-04 22:42:06 +0530

    Day-10, Add two sorted list

---
+++ day-10/src/Main.java
@@ -0,0 +1,94 @@
+//21. Merge Two Sorted Lists
+//
+//Topics
+//        Companies
+//You are given the heads of two sorted linked lists list1 and list2.
+//
+//Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+//
+//Return the head of the merged linked list.
+//
+//
+//
+//Example 1:
+//
+//
+//Input: list1 = [1,2,4], list2 = [1,3,4]
+//Output: [1,1,2,3,4,4]
+//Example 2:
+//
+//Input: list1 = [], list2 = []
+//Output: []
+//Example 3:
+//
+//Input: list1 = [], list2 = [0]
+//Output: [0]
+//
+//
+//Constraint//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+
+        // Example usage of mergeTwoLists function
+        ListNode list1 = new ListNode(1, new ListNode(3, new ListNode(5)));
+        ListNode list2 = new ListNode(2, new ListNode(4, new ListNode(6)));
+
+        ListNode mergedList = mergeTwoLists(list1, list2);
+
+        // Print the merged list
+        System.out.print("Merged List: ");
+        printList(mergedList);
+    }
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(); // Create a dummy node to simplify merging
+        ListNode current = dummy; // Pointer to the current node in the merged list
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        if (list1 != null) {
+            current.next = list1;
+        } else {
+            current.next = list2;
+        }
+
+        return dummy.next;
+    }
+
+    // ListNode class definition
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    // Helper method to print a linked list
+    private static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+}
