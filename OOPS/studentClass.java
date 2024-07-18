d60f9ff66806c3d285beced64aa822ebd7837240
Utkarsh Sharma <utkarshsharma366@gmail.com>
2024-07-18 18:54:26 +0530

    basic java practice for interview

--- OOPS/studentClass.java
+++ OOPS/studentClass.java
@@ -8,14 +8,30 @@ public class studentClass {
         int rno;
         double percent;
     }
+    class Shape{
+        String color = " blue";
+    }
+    class Triangle extends Shape{
+        int sides = 3;
+    }
     public static void main(String[] args) {
 Student x = new Student();
 x.name = "ram";
 x.rno = 1;
 x.percent =92.5;
-System.out.println(x.name);
-    }
-    Scanner sc = new scanner(System.in);
-    int x = sc.nexInt();
 
-}
+
+
+        Student y = new Student();
+        y.name = "ram";
+        y.rno = 1;
+        y.percent =92.5;
+
+System.out.println(x.name);
+        System.out.println(triangle.color);
+
+
+
+
+
+}}
